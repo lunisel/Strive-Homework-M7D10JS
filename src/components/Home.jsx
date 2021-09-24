11106409f9c95c6b6fb47238e5a431489801b2fe
Luna Sarmiento <selene942008@hotmail.com>
2021-09-24 13:30:34 +0200

    start

---
+++ src/components/Home.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+
+const apiKey = "88ac6ac2aa7e480aac6181b2c46dc992";
+
+const Home = () => {
+  const [cityName, setCityName] = useState("Bologna");
+  const [data, setData] = useState(null);
+
+  const fetchCurrent = async () => {
+    try {
+      let response = await fetch(`
+      api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}
+        `);
+      if (response.ok) {
+        console.log("Response is ok");
+        let weather = await response.json();
+        setData(weather);
+        console.log(weather);
+      } else {
+        console.log("Error Error Error");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchCurrent();
+  }, []);
+
+  return (
+    <div className="Home-container">
+      <h1>HELLO</h1>
+    </div>
+  );
+};
+
+export default Home;
